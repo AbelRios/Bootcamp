f4870ee3513a798b910d7b5073acf992be46b5c4
AbelRios <abel.rios.90@gmail.com>
2022-05-25 20:44:08 +0200

    ..

--- React/ejerciciorickmorty/src/components/List/List.js
+++ React/ejerciciorickmorty/src/components/List/List.js
@@ -7,6 +7,7 @@ export default function List({ list }) {
     // Aquí escribir el código
 
     return (
+        // hacer un div que me haga un grid guapo que me ponga al menos 3 o 4 tarjetas por fila
 
     )
     

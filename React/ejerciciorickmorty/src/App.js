c2b257b0ec1ac14dfb10c72f7b42744e68c47f72
AbelRios <abel.rios.90@gmail.com>
2022-05-25 20:40:52 +0200

    react ejRickMorty inicial

--- React/ejerciciorickmorty/src/App.js
+++ React/ejerciciorickmorty/src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+import {useState, useEffect} from "react";
+import List from "./components/List";
 import './App.css';
 
 function App() {
+
+  const [list, setList] = useState(null);
+
+  useEffect(function () {
+
+      async function fetchApi() {
+          const response = await fetch("https://rickandmortyapi.com/api/character");
+          const json = await response.json();
+          setList(json.results); 
+      }
+      fetchApi();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Ejercicio API Rick & Morty</h2>
+        <h3>Abel Rios</h3>
+        <List list={list}/>
       </header>
     </div>
   );
---
+++ React/ejerciciorickmorty/src/components/List/List.js
@@ -0,0 +1,13 @@
+export default function List({ list }) {
+
+    if (!list) {
+        return ("Cargando Lista de Personajes...")
+    } 
+
+    // Aquí escribir el código
+
+    return (
+
+    )
+    
+}
